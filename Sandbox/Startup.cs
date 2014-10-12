8751dd8ac79cc747230006e45db322251869ce54
Jerry Backer <jwb726@gmail.com>
2014-10-12 10:02:42 -0500

    Copied project from local directory.

---
+++ Sandbox/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using VizLab;
+
+[assembly: OwinStartupAttribute(typeof(Startup))]
+namespace VizLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ VizLab.Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace VizLab.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Dataset> Datasets { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class Dataset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
